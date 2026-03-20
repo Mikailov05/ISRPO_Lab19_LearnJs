936a2955800d3ea54237367c469727750c789924
Mikailov05 <ahmed.mikailov@interner.ru>
2026-03-20 10:36:08 +0300

    Step 6: functions in JavaScript

--- main.js
+++ main.js
@@ -28,3 +28,60 @@
            console.log('i = ${i}, j = ${j}');
        }
    }
+
+   function sum(a, b) {
+       return a + b;
+
+   }
+
+   console.log(sum(3, 5));
+
+   function sayHello(name) {
+       console.log('Hello , ${name}');
+
+   }
+   sayHello("Ахмед");
+
+   function greet(name = "Гость") {
+
+       console.log("Привет," + name);
+
+   }
+   greet();
+
+   greet("Анастасия");
+
+   const add = function(a, b) {
+       return a + b;
+
+   };
+   console.log(add(2, 3));
+
+   function makeCounter() {
+       let count = 0;
+       return function() {
+           count++;
+           return count;
+
+       };
+   }
+
+
+   const counter = makeCounter();
+   console.log(counter());
+   console.log(counter());
+   console.log(counter());
+
+   function sumFunc(a, b) {
+       return a, b;
+   }
+   const sumFunc2 = (a, b) => a + b;
+
+   const double = x => x * 2;
+   const sumFunc3 = (a, b) => a + b;
+
+   const sayGreeting = () => console.log("Hello");
+   const square = x => x * x;
+   const calculate = (a, b);
+   let result = a + b;
+   return result * 2;
